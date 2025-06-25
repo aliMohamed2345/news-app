4ae353056acc5a7984861938bf89b08bd62d11cc
Ali Mohamed Mansor <137876199+aliMohamed2345@users.noreply.github.com>
2025-06-25 18:19:55 +0300

    Add navigation components and category pages

---
+++ src/app/utils/data.ts
@@ -0,0 +1,9 @@
+export const newsCategories = [
+  { name: `business`, id: 1 },
+  { name: `entertainment`, id: 2 },
+  { name: `general`, id: 3 },
+  { name: `health`, id: 4 },
+  { name: `science`, id: 5 },
+  { name: `sports`, id: 6 },
+  { name: `technology`, id: 7 }
+];
